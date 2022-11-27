64d32a59cda80ea1e008e9950f5118f3e47c803c
sahmaragaev <sahmaraqayev32@gmail.com>
2022-11-27 13:32:38 +0400

    Finishing main page and adding header to info

---
+++ scripts/app.js
@@ -0,0 +1,23 @@
+import AboutUs from "../views/components/about_us.js";
+import Shahmar from "../views/components/shahmar.js";
+
+const header = document.getElementById('header');
+const info = document.getElementById('info');
+
+header.innerHTML = await AboutUs.render();
+
+const shahmar = document.getElementById('shahmar');
+shahmar.onclick = async function() {
+    //info.innerHTML = await Shahmar.render();
+};
+
+const turkana = document.getElementById('turkana');
+turkana.onclick = async function() {
+
+};
+
+const vaqif = document.getElementById('vaqif');
+vaqif.onclick = async function() {
+
+};
+
