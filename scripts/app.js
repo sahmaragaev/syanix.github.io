12d9bcea6df4eafb7bbca6f9730b89fee2dc806f
sahmaragaev <sahmaraqayev32@gmail.com>
2022-11-27 15:56:16 +0400

    Adding Shahmar info details

--- scripts/app.js
+++ scripts/app.js
@@ -3,12 +3,13 @@ import Shahmar from "../views/components/shahmar.js";
 
 const header = document.getElementById('header');
 const info = document.getElementById('info');
+info.innerHTML = await Shahmar.render();
 
 header.innerHTML = await AboutUs.render();
 
 const shahmar = document.getElementById('shahmar');
 shahmar.onclick = async function() {
-    //info.innerHTML = await Shahmar.render();
+    info.innerHTML = await Shahmar.render();
 };
 
 const turkana = document.getElementById('turkana');
@@ -20,4 +21,3 @@ const vaqif = document.getElementById('vaqif');
 vaqif.onclick = async function() {
 
 };
-
